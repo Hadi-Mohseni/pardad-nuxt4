3562c3c173094a27c17e3bff13a66818258513c9
Hadi Mohseni <hadimohseni1996@gmail.com>
2025-10-08 18:08:13 +0330

    perf: perf: add minimization to enhance performance

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,7 +13,6 @@ export default defineNuxtConfig({
         lang: 'en',
       },
       title: 'گروه پرداد',
-
       script: [
         {src: '/TweenMax.min.js', defer: true},
         {src: '/TimelineMax.min.js', defer: true},
